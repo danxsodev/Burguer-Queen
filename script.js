21c1ff2f5d604d4d65d59b28f7844617ba72acf6
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-05-23 00:26:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,6 @@ function start() {
     document.getElementById("close-menu").style.visibility="hidden";
 }
 
-function onScroll() {
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
-}
-
 function openMenu() {
     document.body.classList.add('menu-expanded');
     document.getElementById("open-menu").style.visibility="hidden";
@@ -23,6 +15,14 @@ function closeMenu() {
     document.body.classList.remove('menu-expanded');
     document.getElementById("close-menu").style.visibility="hidden";
     document.getElementById("open-menu").style.visibility="visible";
+}
+
+function onScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
 }
 
 ScrollReveal({
