aee6e8537d436d1c293ebc908a2e2a343a72c2df
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-06-20 05:17:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function inicialize (onload) {
+onScroll()
+}
+window.onload
+
+/*FUNÇÃO QUE ADICIONA E REMOVE O MENU FIXO AO SCROLLAR*/
 function onScroll() {
     const navigation = document.getElementById("navigation");
     
@@ -9,18 +15,21 @@ function onScroll() {
 }
 window.addEventListener("scroll", onScroll);
 
+/*FUNÇÃO QUE ABRE O MENU*/
 function openMenu() {
     document.body.classList.add("menu-expanded");
     document.getElementById("open-menu").style.visibility = "hidden";
     document.getElementById("close-menu").style.visibility = "visible";
 }
 
+/*FUNÇÃO QUE FECHA O MENU*/
 function closeMenu() {
     document.body.classList.remove("menu-expanded");
     document.getElementById("close-menu").style.visibility = "hidden";
     document.getElementById("open-menu").style.visibility = "visible";
 }
 
+/*ScrollReveal animando o scroll de cima para baixo*/
 ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -44,11 +53,13 @@ ScrollReveal({
 #contato, 
 #contato .wrapper,
 #contato .content,
+#contato .map,
 #faq,
 #faq .wrapper,
 #faq .card,
 #rodape`)
 
+/*ScrollReveal animando o scroll debaixo para cima*/
 ScrollReveal({
     origin: 'bottom',
     distance: '30px',
@@ -58,6 +69,7 @@ ScrollReveal({
 #faq .card,
 #faq .wrapper,
 #faq,
+#contato .map,
 #contato .content,
 #contato .wrapper,
 #contato,
@@ -77,6 +89,7 @@ ScrollReveal({
 #home,
 #navigation`)
 
+/*FAQ*/
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
@@ -85,3 +98,49 @@ cards.forEach((card) => {
     });
 });
 
+/*Light Mode/Dark Mode*/
+const inputContainer = document.querySelector('input');
+const rootElement = document.documentElement;
+
+const lightTheme = {
+    "--primary-color": "#803e00",
+    "--secondary-color":"#ff8c00",
+    "--paragraph-color":"#000000",
+    "--before-color":"#f4e5d7",
+    "--stats-color":"#fffaf0",
+    "--card-color": "#f4e5d7",
+    "--button-color":"#009980",
+    "--button-hover-color":"#004d40", 
+};
+
+const darkTheme = {
+    "--primary-color":"#ff8c00",
+    "--paragraph-color":"#e6e6e6",
+    "--before-color":"#1a1a1a",
+    "--stats-color":"#545454",
+    "--card-color":"#333333",
+    "--button-color":"#009980",
+    "--button-hover-color":"#004d40",
+};
+
+
+inputContainer.addEventListener('change', function() {
+    const isChecked = inputContainer.checked
+
+    if (isChecked) {
+        changeTheme(darkTheme)
+    } else {
+        changeTheme(lightTheme)
+    }
+});
+
+/*FUNÇÃO QUE TROCA O TEMA/MODO*/
+function changeTheme(theme) {
+    for (let prop in theme) {
+        changePropery(prop, theme[prop])
+    }
+}
+
+function changePropery (property, value) {
+    rootElement.style.setProperty(property, value)
+}
